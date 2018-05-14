b171606303b9749ac618de3af70c7bb7e6659830
msabuthahirk <msabuthahirk@gmail.com>
2018-05-14 20:51:49 +0530

    fetched inside the page

--- scrapyProject/spiders/blablacar.py
+++ scrapyProject/spiders/blablacar.py
@@ -1,15 +1,26 @@
 # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals
 import scrapy
-
 
 class BlablacarSpider(scrapy.Spider):
     name = 'blablacar'
-    allowed_domains = ['www.blablacar.in/']
+    # allowed_domains = ['www.blablacar.in/']
     start_urls = ['https://www.blablacar.in/ride-sharing/new-delhi/chandigarh/#?fn=new+delhi/']
 
     def parse(self, response):
-        # link = response.css('.relative a::attr(href)').extract()
+        # yield {
+        #     'link' : response.css('.relative a::attr(href)').extract()
+        # }
+        homepage = "https://www.blablacar.in"
+        urls = response.css('.relative a::attr(href)').extract()
+        for url in urls:
+            myurl = homepage + url
+            yield scrapy.Request(url=myurl, callback=self.parse_details)
+
+    def parse_details(self, response):
         yield {
-            'link' : response.css('.relative a::attr(href)').extract()
+            'location' : response.css('.RideName-location--arrowAfter::text').extract_first(),
+            'node' : response.css('.Footer-footnotes .u-marginNone::text').extract(),
         }
+
+# response.css('.RideName-location--arrowAfter::text').extract_first()
+# response.css('.Footer-footnotes .u-marginNone::text').extract()
