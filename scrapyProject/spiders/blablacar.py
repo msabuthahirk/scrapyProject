1117e82f83b1c5620fcb979188069d4479282de0
msabuthahirk <msabuthahirk@gmail.com>
2018-05-15 10:29:01 +0530

    final

--- scrapyProject/spiders/blablacar.py
+++ scrapyProject/spiders/blablacar.py
@@ -3,7 +3,7 @@ import scrapy
 
 class BlablacarSpider(scrapy.Spider):
     name = 'blablacar'
-    # allowed_domains = ['www.blablacar.in/']
+    # allowed_domains = ['www.blablacar.in']
     start_urls = ['https://www.blablacar.in/ride-sharing/new-delhi/chandigarh/#?fn=new+delhi/']
 
     def parse(self, response):
